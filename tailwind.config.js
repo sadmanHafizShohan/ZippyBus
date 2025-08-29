e4afbec94c80cedf94cf854b00ed44b76630cdd2
snrshohan <snrshohan21@gmail.com>
2025-08-30 04:10:20 +0600

    animation added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Specify paths to your template files
+  ],
+  theme: {
+    extend: {}, // Extend default styles here
+  },
+  plugins: [], // Add plugins if needed
+};
